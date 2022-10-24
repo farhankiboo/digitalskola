1cea6986f096389687a5bdbcd33452268cf8616c
Farhan Ferdianto <farhan.kibo@gmail.com>
2022-10-24 22:39:43 +0700

    java

---
+++ StudentApp.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        Academician academician = new Academician();
+
+        // Call object
+        Academician academician = new Academician();
+        academician.name = "Farhan Ferdianto";
+        academician.age = "32";
+
+        // Call constructor
+        System.out.println(academician.name);
+        System.out.println(academician.age);
+
+        // Student.studentSubject("Farhan Ferdianto");
+        
+        // Academician academician = new Academician("Farhan Ferdianto", "32");
+        
+        // Student student = new Student(name: "Farhan Ferdianto");
+        // student.name = "Farhan Ferdianto";
+
+
+        // Rectangle rectangle = new Rectangle();
+        // System.out.println(rectangle.getCorner());
+        // System.out.println(rectangle.getParentCorner());
+    }
+
+}
